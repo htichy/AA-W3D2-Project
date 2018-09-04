7c6216a580c42d7748db5339d1d417ecfe488f92
htichy <haven.tichy@gmail.com>
2018-09-04 11:29:26 -0700

    Create seeded database add gemfile

--- aA_questions/import_db.sql
+++ aA_questions/import_db.sql
@@ -1,3 +1,11 @@
+DROP TABLE IF EXISTS question_tags;
+DROP TABLE IF EXISTS question_follows;
+DROP TABLE IF EXISTS question_likes;
+DROP TABLE IF EXISTS replies;
+DROP TABLE IF EXISTS questions;
+DROP TABLE IF EXISTS tags;
+DROP TABLE IF EXISTS users;
+
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE users (
@@ -10,7 +18,7 @@ CREATE TABLE questions (
   id INTEGER PRIMARY KEY,
   title TEXT NOT NULL,
   body TEXT NOT NULL,
-  user_id INTEGER NOT NULL
+  user_id INTEGER NOT NULL,
   
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
@@ -25,18 +33,18 @@ CREATE TABLE question_follows (
 
 CREATE TABLE replies (
   id INTEGER PRIMARY KEY,
-  parent_id INTEGER,
+  parent_reply_id INTEGER,
   user_id INTEGER NOT NULL,
   body TEXT NOT NULL,
   question_id INTEGER NOT NULL,
   
-  FOREIGN KEY (parent_id) REFERENCES replies(id),
+  FOREIGN KEY (parent_reply_id) REFERENCES replies(id),
   FOREIGN KEY (user_id) REFERENCES users(id),
   FOREIGN KEY (question_id) REFERENCES questions(id)
 );
 
 CREATE TABLE question_likes (
-  id INTEGER PRIMARY KEY
+  id INTEGER PRIMARY KEY,
   user_id INTEGER NOT NULL,
   question_id INTEGER NOT NULL,
   
@@ -57,7 +65,7 @@ VALUES
   ("Ned", "Ruggeri"), ("Kush", "Patel"), ("Earl", "Cat");
   
 INSERT INTO
-  questions (title, body, author_id)
+  questions (title, body, user_id)
 SELECT
   "Kush Question", "KUSH KUSH KUSH", users.id
 FROM
@@ -66,7 +74,7 @@ WHERE
   users.fname = "Kush" AND users.lname = "Patel";
 
 INSERT INTO
-  questions (title, body, author_id)
+  questions (title, body, user_id)
 SELECT
   "Earl Question", "MEOW MEOW MEOW", users.id
 FROM
@@ -85,7 +93,7 @@ VALUES
 );
 
 INSERT INTO
-  replies (question_id, parent_reply_id, author_id, body)
+  replies (question_id, parent_reply_id, user_id, body)
 VALUES
   ((SELECT id FROM questions WHERE title = "Earl Question"),
   NULL,
@@ -94,7 +102,7 @@ VALUES
 );
 
 INSERT INTO
-  replies (question_id, parent_reply_id, author_id, body)
+  replies (question_id, parent_reply_id, user_id, body)
 VALUES
   ((SELECT id FROM questions WHERE title = "Earl Question"),
   (SELECT id FROM replies WHERE body = "Did you say NOW NOW NOW?"),
